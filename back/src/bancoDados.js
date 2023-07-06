42e5295a86d4a769cb2795831f66dd86be8f16f8
AlexandreAMMB <31141564+AlexandreAMMB@users.noreply.github.com>
2023-07-05 22:20:43 -0300

    first commit

---
+++ back/src/bancoDados.js
@@ -0,0 +1,113 @@
+const fs = require('fs')
+//carrega o arquivo JSON
+const produtos = require('./produtos')
+
+//retorna todo o arquivo JSON
+function getProdutos() {
+    return produtos
+}
+
+//retorna 1 produto do arquivo JSON
+function getProduto(codigo) {
+    for(const produto of produtos) {
+        if(produto.codigo == codigo) {
+            return produto || {}
+        }
+    }
+}
+
+//Salva um produto na no final do arquivo JSON
+function salvarProduto(produto) {
+
+    if(produtos.includes(produto)) {
+        produtos.indexOf(produto)
+
+        fs.writeFile("produtos.json", JSON.stringify(produtos), err => {
+
+            if(err) throw err
+    
+            console.log("Produto Salvo!")
+        })
+    } else {
+        //Insere o produto novo no buffer do arquivo
+        produtos.push(produto)
+
+        //Sobrescreve o arquivo com a nova lista de produtos
+        fs.writeFile("produtos.json", JSON.stringify(produtos), err => {
+
+            if(err) throw err
+
+            console.log("Produto Salvo!")
+        })
+    }
+
+    return produto
+}
+
+//Exclui do arquivo JSON o produto com o codigo informado
+function excluirProduto(codigo) {
+
+    var produtoRemovido = {}
+
+    //controlador para caso o codigo N seja encontrado=
+    var signal = 1
+
+    //procura o produto com o codigo informado
+    for(const produto of produtos) {
+        if(produto.codigo == codigo) {
+            produtos.splice(produtos.indexOf(produto), 1)
+            produtoRemovido = produto
+            signal = 0
+            break
+        }
+    }
+
+    //caso onde o código não é encontrado
+    if(signal) {
+        console.log(`Produto com código ${codigo} não foi encontrado!`)
+        return {}
+    }
+
+    //Sobrescreve o arquivo com a nova lista de produtos
+    fs.writeFile("produtos.json", JSON.stringify(produtos), err => {
+
+        if(err) throw err
+
+        console.log("Produto Excluído!")
+    })
+
+    return produtoRemovido
+}
+
+/*
+function escrever(){
+
+    //cria um produto
+    let produto = {
+        codigo: 11111,
+        nome: "Novo Produto",
+        preco: 21        
+    }
+
+    //inclui um produto
+    produtos.push(produto)
+
+    //escreve no arquivo json
+    fs.writeFile("produtos.json", JSON.stringify(produtos), err => {
+        if(err) throw err
+
+        console.log("Done writing")
+    })
+
+    console.log(produtos)
+}
+*/
+
+//exporta a função
+module.exports = {
+    //escrever
+    getProdutos,
+    getProduto,
+    salvarProduto,
+    excluirProduto
+}
---
+++ back/src/servidor.js
@@ -0,0 +1,64 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+//excluir dps
+const bodyParser = require('body-parser') //para usar o body do postman
+const fs = require('fs')
+const base64 = require('base64-js')
+
+const bancoDados = require('./bancoDados')
+
+//exluir dps
+app.use(bodyParser.urlencoded({ extended: true }))//Utilização do body parser para conversão de linhas de codigo do body para um objeto que pode ser lido pelo postman
+app.use(bodyParser.json())
+
+//Retorna todos os produtos cadastrados
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDados.getProdutos())
+    //convertida em string JSON
+})
+
+//Retorna 1 produto
+app.get('/produtos/:codigo', (req, res, next) => {
+    res.send(bancoDados.getProduto(req.params['codigo']))
+    //convertida em string JSON
+})
+
+//Salva 1 produto
+app.post('/produtos',(req, res, next) => {
+    // Lê a imagem como um ArrayBuffer
+    const imagemBuffer = fs.readFileSync("../figura1empreendedorismo.png")
+
+    console.log(imagemBuffer)
+
+    // Converte o ArrayBuffer em um Uint8Array
+    const imagemArray = new Uint8Array(imagemBuffer)
+
+    // Converte o Uint8Array em uma string Base64
+    const imagemBase64 = base64.fromByteArray(imagemArray)
+
+    const produto = bancoDados.salvarProduto({
+        codigo: req.body.codigo,
+        nome: req.body.nome,
+        descricao: req.body.descricao,
+        preco: req.body.preco,
+        imagem: imagemBase64
+    })
+    //res.send("O produto ${produto.nome} foi inserido");
+    res.send(produto)
+})
+
+//Exclui 1 produto
+app.delete('/produtos/:codigo', (req, res) =>{
+    const produto = bancoDados.excluirProduto(req.params['codigo'])
+    res.send(produto)
+})
+
+app.use((req, res, next) => {
+    res.send("URL não reconhecida")
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor agora executando na porta ${porta}.`)
+})
